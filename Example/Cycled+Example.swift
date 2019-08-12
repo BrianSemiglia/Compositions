eb3dc673003a55023c97f42c0130f58ba1e498a7
Brian Semiglia <brian.semiglia@gmail.com>
2019-08-11 20:26:09 -0400

    Adds async example.

--- Example/Cycled+Example.swift
+++ Example/Cycled+Example.swift
@@ -10,9 +10,9 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-extension UILabel {
-    static func example() -> Cycled<(UIView, Void), String> {
-        return Cycled { stream in
+extension UIView {
+    static func animated() -> Cycled<(UIView, Void), String> { return
+        Cycled { stream in
             let viewer = stream.lens(
                 get: { s -> UILabel in
                     let x = UILabel()
@@ -55,4 +55,117 @@ extension UILabel {
             return viewer.zip(animator)
         }
     }
+    
+    static func async() -> Cycled<(BlueToothAdapter, UIViewController), State> { return
+        Cycled { source in
+            let bluetooth = source.lens(
+                get: { state in
+                    BlueToothAdapter().rendering(state) { adapter, state in
+                        if state.transmission == .sending {
+                            _ = adapter.sending(state.text)
+                        }
+                    }
+                },
+                set: { adapter, state in
+                    adapter
+                        .bytes()
+                        .withLatestFrom(state) { ($0, $1) }
+                        .map { response, state in
+                            var new = state
+                            new.responses = state.responses + [response]
+                            new.transmission = .idle
+                            return new
+                    }
+                }
+            )
+            
+            let view = Lens<Observable<State>, (UITextView, UITextView)>(
+                value: source,
+                get: { state in (
+                    UITextView().rendering(state) { view, state in
+                        view.frame = .init(
+                            origin: .init(x: 30, y: 60),
+                            size: .init(width: 300, height: 44)
+                        )
+                        view.text = state.text
+                        view.backgroundColor = state.text.count % 2 == 0 ? .yellow : .red
+                    },
+                    UITextView().rendering(state) { view, state in
+                        view.frame = .init(
+                            origin: .init(x: 30, y: 130),
+                            size: .init(width: 300, height: 600)
+                        )
+                        view.text = state.responses.reduce("") { $0 + "\n" + $1 }
+                    }
+                )},
+                set: { views, state -> Observable<State> in
+                    views
+                        .0
+                        .rx
+                        .text
+                        .map { $0! }
+                        .withLatestFrom(state) { ($0, $1) }
+                        .filter { $0 != $1.text }
+                        .map { text, state in
+                            var new = state
+                            new.text = text
+                            new.transmission = .sending
+                            return new
+                        }
+                }
+            )
+            
+            let viewController = view.map { state, views -> UIViewController in
+                let x = UIViewController()
+                x.view.backgroundColor = .blue
+                x.view.addSubview(views.0)
+                x.view.addSubview(views.1)
+                return x
+            }
+            
+            let composed = bluetooth
+                .zip(viewController)
+                .prefixed(
+                    with: .just(
+                        State(
+                            text: "Hello World",
+                            responses: [],
+                            transmission: .idle
+                        )
+                    )
+                )
+            
+            return composed
+        }
+    }
+}
+
+final class BlueToothAdapter: NSObject {
+    
+    private let output = PublishSubject<String>()
+    
+    func sending(_ input: String) -> BlueToothAdapter {
+        output.on(.next(input + " response"))
+        return self
+    }
+    
+    func bytes() -> Observable<String> { return
+        output
+            .asObservable()
+            .delay(
+                .seconds(1),
+                scheduler: MainScheduler()
+            )
+    }
+}
+
+struct State: Equatable {
+    enum TransmissionState: Equatable {
+        case idle
+        case sending
+        case receiving
+    }
+    var text: String
+    var responses: [String]
+    var transmission: TransmissionState
 }
